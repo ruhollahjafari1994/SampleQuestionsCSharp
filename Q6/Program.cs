1e5787b687207e180317d4dca12f78321db05cf2
ruhollah_jafari <jruhollah@gmail.com>
2020-12-03 20:25:45 +0330

    لیست تمامی برنامه های در حال اجرا روی ویندوز

---
+++ Q6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            System.Diagnostics.Process CurrentProc = System.Diagnostics.Process.GetCurrentProcess();
+            System.Diagnostics.Process[] proc = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process item in proc)
+            {
+                i++;
+                Console.WriteLine("item Number " + i + " Of  " + item);
+                Console.WriteLine("------------------------------------------------------------------------------------------------****************");
+            }
+            Console.WriteLine(" Count Of Running Programms Now :=> " + i);
+
+
+        }
+    }
+}
