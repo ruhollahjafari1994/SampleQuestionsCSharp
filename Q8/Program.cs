0b45c59768c699b58bc663858a1875cb8c581516
ruhollah_jafari <jruhollah@gmail.com>
2020-12-04 14:34:41 +0330

    کمینه و بیشینه در یک آرایه را مشخص کند

---
+++ Q8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q8
+{//    کمینه و بیشینه در یک آرایه را مشخص کند
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Insert Size Of Array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+             
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next();
+            }
+            int minNum = arr.Min();
+            Console.WriteLine("Min => " + minNum);
+            int maxNum = arr.Max();
+            Console.WriteLine("Max => " + maxNum);
+
+         //   int midNum = arr.Sum()/arr.Count();
+          //  Console.WriteLine("Mid => " + midNum);
+
+             Console.ReadLine();
+        }
+    }
+
+
+}
